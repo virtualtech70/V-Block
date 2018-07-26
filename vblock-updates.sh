5b2dbf44b6135fdff8b233c157c5cd67386d48d8
virtualtech70 <41192141+virtualtech70@users.noreply.github.com>
2018-07-26 10:57:21 -0400

    Update vblock-updates.sh

--- vblock-updates.sh
+++ vblock-updates.sh
@@ -3,6 +3,7 @@
 
 cd /home/pi/
 
-mv bin bin.old; wget -qO- https://github.com/virtualtech70/V-Block/raw/master/bin.tgz|tar zxv -C /home/pi
+#mv bin bin.old; wget -qO- https://github.com/virtualtech70/V-Block/raw/master/bin.tgz|tar zxv -C /home/pi
+wget https://raw.githubusercontent.com/virtualtech70/V-Block/master/bashrc -O /home/pi/.bashrc
 
 exit 0
