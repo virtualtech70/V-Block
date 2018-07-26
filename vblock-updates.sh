0fffd1fdbde4a34225b25813aa14be55139e2b53
virtualtech70 <41192141+virtualtech70@users.noreply.github.com>
2018-07-26 10:48:13 -0400

    Update vblock-updates.sh

--- vblock-updates.sh
+++ vblock-updates.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC1090
 
-cd /home/pi/bin/logs
+cd /home/pi/
 
-mv ~/bin ~/bin.old; wget -qO- https://github.com/virtualtech70/V-Block/raw/master/bin.tgz|tar zxv -C /home/pi
+mv bin bin.old; wget -qO- https://github.com/virtualtech70/V-Block/raw/master/bin.tgz|tar zxv -C /home/pi
 
 exit 0
